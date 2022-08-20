6c9c65e4bfc9132220d758a4c23258fd952e48fc
ManavDaggula <manavdaggula@gmail.com>
2022-08-20 22:24:45 +0530

    basic functioning done

---
+++ script.js
@@ -0,0 +1,80 @@
+// window.alert("loaded")
+
+let fname=document.getElementById('fname');
+let lname=document.getElementById('lname');
+let email=document.getElementById('email');
+let dob=document.getElementById('dob');
+let gender=document.getElementById('gender')
+let photo=document.getElementById('file')
+
+function validate(){
+    
+    // gender need not be checked as a gender will always be selected due to mentioned checked attribute.
+
+    //now check for name validity 🔽
+    if(!(/^[A-Za-z]+$/.test(fname.value.trim()))){
+        window.alert("Enter valid fname.")
+        return false;
+    }
+    if(!(/^[A-Za-z]+$/.test(lname.value.trim()))){
+        window.alert("Enter valid lname.")
+        return false;
+    }
+
+    //to check if dob is mentioned 🔽
+    if(dob.value==""){
+        window.alert("Enter valid Date of Birth.")
+        return false;
+    }
+
+    // to check if email is correctly mentioned 🔽
+    if(email.value=="" || !(/[A-Za-z0-9]@[a-z].[a-z]/.test(email.value))){
+        window.alert("Please enter valid email.")
+        return false;
+    }
+
+    //to check if a photo is uploaded 🔽
+    if(photo.value===""){
+        window.alert("Please attach an image.")
+        return false;
+    }
+
+    return true;
+
+}
+
+function clear(){
+    console.log("clear called.")
+    let l = document.querySelectorAll('.left input[type="text"],.left input[type="email"],.left input[type="date"],.left input[type="file"]')
+    for (let i=0;i<l.length;i++){
+        l[i].value="";
+    }
+}
+
+function add(){
+    if(!validate()){return}
+    let newRow = document.createElement('tr');
+    let newImg = document.createElement('td');
+    let newName = document.createElement('td');
+    let newGender = document.createElement('td');
+    let newEmail = document.createElement('td');
+    let newDOB = document.createElement('td');
+
+    //now setting the details
+    newName.textContent = fname.value +" "+ lname.value;
+    newImg.innerHTML="<img src="+URL.createObjectURL(photo.files[0])+">";
+    newGender.textContent = gender.querySelector('input:checked').value
+    newDOB.textContent=dob.value;
+    newEmail.textContent=email.value;
+
+    newRow.append(newImg);
+    newRow.append(newName);
+    newRow.append(newDOB);
+    newRow.append(newEmail);
+    newRow.append(newGender);
+    document.getElementById('display-table').append(newRow);
+    window.alert("Student added.")
+}
+
+document.getElementById('submit').addEventListener('click',add)
+document.getElementById('clear').addEventListener('click',clear);
